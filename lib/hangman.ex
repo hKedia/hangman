9a2db919b900dcbc28068ce8b1850bf5e9e018f6
harsh.kedia <harsh.kedia@alcemy.tech>
2021-08-28 16:24:25 +0200

    delegate make_move, update tests

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -1,13 +1,7 @@
 defmodule Hangman do
-
   alias Hangman.Game
 
-  defdelegate new_game(),             to: Game
-  defdelegate tally(game),            to: Game
-
-  def make_move(game, guess) do
-    game = Game.make_move(game, guess)
-    { game, tally(game) }
-  end
-
+  defdelegate new_game(), to: Game
+  defdelegate tally(game), to: Game
+  defdelegate make_move(game, guess), to: Game
 end
